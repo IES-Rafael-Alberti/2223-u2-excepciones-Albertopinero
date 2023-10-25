12bddb79d73c944b7161cf584d53d3051966914b
Alberopinero <apinero@iesrafaelalberti.es>
2023-10-25 20:33:23 +0200

    Commit ejercicios.3

--- test/test_ejercicio1.py
+++
@@ -1,5 +0,0 @@
-from src.ejercicio1 import CumplirAños
-
-def test_CumplirAños():
-    años = 7
-    assert CumplirAños(años) == "Has cumplido 0 años"
--- test/test_ejercicio5.py
+++
@@ -1,5 +0,0 @@
-from src.ejercicio5 import CompruebaContraseña
-
-def test_CompruebaContraseña():
-    password = "alberto23"
-    assert CompruebaContraseña(password) == "Contraseña Correcta!"
